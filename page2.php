814029c77a18478001aefcaecd59e0bf5ad1d726
Sheck Obidullah <65061871+obidullah01@users.noreply.github.com>
2021-06-22 11:00:34 +0600

    conversion rate page

---
+++ page2.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Conversion Rate</title>
+</head>
+<body>
+    <p><font color="red">Page 1 [Conversion Rate]</font></p>
+    <p><font color="red">Conversion Site</font></p>
+    <nav>
+        <a href="./page1.php">1. Home</a>
+        <a href="./page2.php">2. Conversion Rate</a>
+        <a href="./page3.php">3. History</a>
+    </nav>
+    <br/>
+    <p><font color="red">Conversion Rate</font></p>
+    <table border="1px">
+        <tr>
+            <td>feet to inch</td>
+            <td>1</td>
+            <td>12</td>
+        </tr>
+        <tr>
+            <td>inch to feet</td>
+            <td>0.08</td>
+            <td>1</td>
+        </tr>
+        <tr>
+            <td>feet to centimeter</td>
+            <td>1</td>
+            <td>30.5</td>
+        </tr>
+        <tr>
+            <td>centimeter to feet</td>
+            <td>0.03</td>
+            <td>1</td>
+        </tr>
+        <tr>
+            <td>inch to centimeter</td>
+            <td>1</td>
+            <td>2.54</td>
+        </tr>
+
+
+    </table>
+</body>
+</html>
